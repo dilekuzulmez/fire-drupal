d992d3fefe85217da20fa74ad8b7262dbeae692e
1forh <1forhlol@gmail.com>
2018-11-27 09:21:38 -0500

    formats ASSET_PATH correctly

--- fire-constants.config.js
+++ fire-constants.config.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = {
   PROXY_URL: 'http://fire-theme.fire',
   DEFAULT_PORT: 3000,
-  ASSET_PATH: './assets',
+  ASSET_PATH: path.resolve(__dirname, './assets'),
   DESTINATION_PATH: path.resolve(__dirname, './dist'),
 };
