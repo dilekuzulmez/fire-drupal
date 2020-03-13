e1daa8a2c43adc3e53ce3d85bb2d08999b59484e
John Young <john@skycatchfire.com>
2020-03-12 23:11:46 -0400

    udpates

--- tailwind.config.js
+++ tailwind.config.js
@@ -120,19 +120,27 @@ module.exports = {
       function ColorShades(hex, lum) {
         hex = String(hex).replace(/[^0-9a-f]/gi, '');
         if (hex.length < 6) {
-          hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+          hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
         }
         lum = lum || 0;
-        var rgb = '#',
-          c,
-          i;
+        var rgb = "#", c, i;
         for (i = 0; i < 3; i++) {
-          c = parseInt(hex.substr(i * 2, 2), 16);
-          c = Math.round(Math.min(Math.max(0, c + c * lum), 255)).toString(16);
-          rgb += ('00' + c).substr(c.length);
+          c = parseInt(hex.substr(i*2,2), 16);
+          c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+          rgb += ("00"+c).substr(c.length);
         }
         return rgb;
       }
-    }),
+
+      const colorLight = _.map(config('theme.colors'), (value, key) => {
+        return {
+          [`.${e(`colors-${key}-light`)}`]: {
+            ColorShades(`${value}`, 0.2);
+          }
+        }
+      });
+
+      addUtilities(colorLight);
+    })
   ],
 };
